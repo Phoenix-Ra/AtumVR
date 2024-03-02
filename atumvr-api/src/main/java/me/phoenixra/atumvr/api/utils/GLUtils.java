fc1dd7c8a7fe6e6a90d0389b6b65f1d1004c6d2b
fenix <bogdan_t_1102@mail.ru>
2024-03-02 16:46:18 +0100

    whoops

--- atumvr-api/src/main/java/me/phoenixra/atumvr/api/utils/GLUtils.java
+++ atumvr-api/src/main/java/me/phoenixra/atumvr/api/utils/GLUtils.java
@@ -3,7 +3,7 @@ package me.phoenixra.atumvr.api.utils;
 import org.lwjgl.opengl.GL11;
 
 public class GLUtils {
-    protected void checkGLError(String message) {
+    public static void checkGLError(String message) {
         int error = GL11.glGetError();
         if (error != 0) {
             throw new RuntimeException(message+" OpenGL Error Code: " + error);
