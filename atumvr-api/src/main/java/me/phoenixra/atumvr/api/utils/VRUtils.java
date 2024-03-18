e22243ad575fb0542b7395d6e55c6706ca9d4695
fenix <bogdan_t_1102@mail.ru>
2024-03-18 11:51:24 +0100

    added methods to get error message from VR error codes

--- atumvr-api/src/main/java/me/phoenixra/atumvr/api/utils/VRUtils.java
+++ atumvr-api/src/main/java/me/phoenixra/atumvr/api/utils/VRUtils.java
@@ -13,4 +13,49 @@ public class VRUtils {
                 0f, 0f, 0f, 1f
         );
     }
+
+
+
+    public static String getInputErrorMessage(int error){
+        if(error == 0) return "VRInputError_None";
+        if(error == 1) return "VRInputError_NameNotFound";
+        if(error == 2) return "VRInputError_WrongType";
+        if(error == 3) return "VRInputError_InvalidHandle";
+        if(error == 4) return "VRInputError_InvalidParam";
+        if(error == 5) return "VRInputError_NoSteam";
+        if(error == 6) return "VRInputError_MaxCapacityReached";
+        if(error == 7) return "VRInputError_IPCError";
+        if(error == 8) return "VRInputError_NoActiveActionSet";
+        if(error == 9) return "VRInputError_InvalidDevice";
+        if(error == 10) return "VRInputError_InvalidPriority";
+        if(error == 11) return "VRInputError_InvalidBoneCount";
+        if(error == 12) return "VRInputError_InvalidCompressedData";
+        if(error == 13) return "VRInputError_NoData";
+        if(error == 14) return "VRInputError_BufferTooSmall";
+        if(error == 15) return "VRInputError_MismatchedActionManifest";
+        if(error == 16) return "VRInputError_MissingSkeletonData";
+        if(error == 17) return "VRInputError_InvalidBoneIndex";
+        if(error == 18) return "VRInputError_InvalidPriority";
+        if(error == 19) return "VRInputError_PermissionDenied";
+        if(error == 20) return "VRInputError_WrongType";
+        return "Unknown";
+    }
+    public static String getCompositorErrorMessage(int code) {
+        if(code == 0) return "VRCompositorError_None";
+        if(code == 1) return "VRCompositorError_RequestFailed";
+        if(code == 100) return "VRCompositorError_IncompatibleVersion";
+        if(code == 101) return "VRCompositorError_DoNotHaveFocus";
+        if(code == 102) return "VRCompositorError_InvalidTexture";
+        if(code == 103) return "VRCompositorError_IsNotSceneApplication";
+        if(code == 104) return "VRCompositorError_TextureIsOnWrongDevice";
+        if(code == 105) return "VRCompositorError_TextureUsesUnsupportedFormat";
+        if(code == 106) return "VRCompositorError_SharedTexturesNotSupported";
+        if(code == 107) return "VRCompositorError_IndexOutOfRange";
+        if(code == 108) return "VRCompositorError_AlreadySubmitted";
+        if(code == 109) return "VRCompositorError_InvalidBounds";
+        if(code == 110) return "VRCompositorError_AlreadySet";
+        return "Unknown";
+    }
+
+
 }
